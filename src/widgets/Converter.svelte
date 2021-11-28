40f481856147096f2a3ef9169370b0befe37f811
Paqui <paqui.calabria@adevinta.com>
2021-11-28 19:35:19 +0100

    feat (rate-info): show conversion rate

--- src/widgets/Converter.svelte
+++ src/widgets/Converter.svelte
@@ -10,6 +10,9 @@
   $: convertedValue = (entryCurrency && convertCurrency && rates[entryCurrency] && rates[convertCurrency])
     ? (entryValue * rates[entryCurrency] / rates[convertCurrency]).toFixed(2)
     : 0
+  $: rate = (entryCurrency !== convertCurrency)
+    ? (rates[entryCurrency] / rates[convertCurrency]).toFixed(5)
+    : 0
 
 </script>
 <div class="etc-currency-converter">
@@ -17,9 +20,13 @@
   <div class="etc-currency-converter__content">
     <NumberInput step={0.01} bind:value={entryValue}/>
     <CurrencySelect bind:value={entryCurrency}/>
-    <!-- TODO: show rate -->
     <span>=</span>
     <span class="etc-currency-converter__result">{ convertedValue }</span>
     <CurrencySelect bind:value={convertCurrency}/>
+    {#if rate}
+      <div class="etc-currency-converter__rate-info" >
+        <span>Conversion rate is <span class="etc-currency-converter__rate-value">{rate}</span></span>
+      </div>
+    {/if}
   </div>
 </div>
